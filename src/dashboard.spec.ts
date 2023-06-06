039673bcb30f13de5b54cd19d7c93b074a74a003
Byambajav <byambajav.munkhbayar@erin.systems>
2023-06-06 13:00:06 +0800

    feat: add dashboard test

---
+++ src/dashboard.spec.ts
@@ -0,0 +1,20 @@
+import { test } from "@playwright/test";
+
+const authFile = "playwright/.auth/user.json";
+
+test("dashboard", async ({ page }) => {
+  await page.goto("https://test-lms.erin.systems/#/login");
+  await page.fill('input[placeholder="Хэрэглэгчийн нэр"]', "playwright");
+  await page.fill('input[type="Нууц үг"]', "Secret123");
+  await page.click("button");
+  await page.waitForURL("https://test-lms.erin.systems/#/dashboard");
+  await page.isVisible("text=ЦАХИМ СУРГАЛТ");
+  await page.isVisible("text=Нийт суралцагчид");
+  await page.isVisible("text=90% танилцсан");
+  await page.isVisible("text=Танилцсан дундаж хувь");
+  await page.isVisible("text=Цахим сургалттай танилцсан хугацаа");
+  await page.isVisible("text=Алба хэлтэс");
+  await page.isVisible("text=Эхлэх");
+  await page.isVisible("text=Дуусгах");
+  await page.context().storageState({ path: authFile });
+});
